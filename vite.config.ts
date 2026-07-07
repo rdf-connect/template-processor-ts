c4ed58294aa8db8ca443bf5a159c7327b71fff22
smessie <smessie@smessie.com>
2026-07-07 14:32:17 +0200

    chore(deps): Remove vite-tsconfig-paths and update config to use tsconfigPaths option

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
     test: {
         deps: {
             optimizer: {
@@ -12,5 +10,8 @@ export default defineConfig({
                 },
             },
         },
+    },
+    resolve: {
+        tsconfigPaths: true,
     },
 });
